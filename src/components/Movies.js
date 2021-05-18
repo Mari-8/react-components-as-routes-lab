fad3d55087b8d6ad4572c77577c16f21e140a591
Mari-8 <ryanaltimari8@gmail.com>
2021-05-18 12:35:17 -0500

    commit tree 75a9e8f20ab11b66aac11ddcf7c164879794165c

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,9 +4,20 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map(mov => {
+        return (
+        <div>
+          {mov.title} 
+          {mov.time}
+          <ul>
+            {mov.genres.map(gen => <li>{gen}</li>)}
+          </ul>
+        </div>
+        )
+      })}
     </div>
-  );
-};
+  )
+}
 
-export default Movies;
+export default Movies
